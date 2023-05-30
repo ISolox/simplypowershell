96bf45d1b53cb649173a5e0286bf54eb6b24f24b
ISolox <brendenleftwich@gmail.com>
2023-05-30 11:53:51 -0400

    Create MountEXT4.ps1
    This is a simple script to mount EXT4 drives via Windows Subsystem for Linux. It will not work unless you have WSL installed (https://learn.microsoft.com/en-us/windows/wsl/install)
    
    This was originally created to mount my External EXT4 drive from my Steam Deck to Windows to access the games on it.
    
    You'll access the drive similar to a network share. The path for the drive once mounted will be the following unless modified:
    
    \\wsl.localhost\LINUXDISTROHERE\mnt\wsl\YOURDRIVEHERE

---
+++ WSL/MountEXT4.ps1
@@ -0,0 +1,35 @@
+#This script is a very basic way to mount EXT4 drives in Windows via WSL. This is a very jank scipt and not professional at all.
+#I created this to use on external drive between a Windows 11 system and a Steam Deck, but it should work with other EXT4 drives.
+
+#Change \\.\PHYSICALDRIVE1 to the EXT4 drive listed by entering "GET-CimInstance -query "SELECT * from Win32_DiskDrive" in PowerShell if needed:
+$EXT4Drive = "\\.\PHYSICALDRIVE1"
+
+
+#This gives a UAC prompt to run as an Administrator:
+Function Check-RunAsAdministrator()
+{
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       Write-host "Script is running with Administrator privileges!"
+  }
+  else
+    {
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+ 
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+ 
+       $ElevatedProcess.Verb = "runas"
+ 
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+ 
+       Exit
+ 
+    }
+}
+Check-RunAsAdministrator
+
+#The actual mount command:
+wsl --mount $EXT4Drive --partition 1
+pause
